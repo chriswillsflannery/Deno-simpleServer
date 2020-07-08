441df3ab42e825c6feee093e2aadb9cd548d613f
Chris Flannery <chriswillsflannery@gmail.com>
2020-07-07 21:26:41 -0400

    add files:

---
+++ controllers/products.ts
@@ -0,0 +1,23 @@
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+}
+
+const products: Product[] = [{
+  id: 1,
+  name: "Product One",
+  description: "This is one",
+  price: 29.99,
+}, {
+  id: 2,
+  name: "Product two",
+  description: "This is two",
+  price: 22.99,
+}, {
+  id: 3,
+  name: "Product three",
+  description: "This is three",
+  price: 295.99,
+}];
